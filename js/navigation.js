1dfe93ef87ef97742d2c10df16dacb2767b5e01f
kilasi <joseph.kilasi@incentro.com>
2018-08-18 18:06:50 +0300

    added comments

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function ($) {
     $('.slicknav_icon').click(function () {
+        // used to show the mobile menu on mobile only
         $('.mobile-nav').slideToggle('display-none');
     });
 });
