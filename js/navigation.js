332bb85f20244978c52083294e11218110f28f7b
kilasi <joseph.kilasi@incentro.com>
2018-08-18 18:06:44 +0300

    remove unused code

--- js/navigation.js
+++ js/navigation.js
@@ -1,21 +1,5 @@
-    jQuery(document).ready(function ($) {
-        $('.slicknav_icon').click(function () {
-            $('.mobile-nav').slideToggle('display-none');
-        });
-        // var url = window.location.pathname, // in real app this would have to be replaced with window.location.pathname
-        //     urlRegExp = new RegExp(url.replace(/\/$/, '')); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
-       
-        // var urlContainsId = urlRegExp.toString().includes("#");
-        // // now grab every link from the navigation
-        // $('.mobile-nav a').each(function () {
-        //     // and test its href against the url pathname regexp
-        //     if (urlRegExp.test(this.href)) {
-        //         if (urlContainsId == false) {
-                    
-        //             $(this).addClass('active');
-        //         }
-        //     }
-        // });
-
-
+jQuery(document).ready(function ($) {
+    $('.slicknav_icon').click(function () {
+        $('.mobile-nav').slideToggle('display-none');
     });
+});
